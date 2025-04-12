ac3a9f31484094b8f49b0b6eb8fe3e8be45b74be
dan95 el guay <ddm19dev@gmail.com>
2025-04-12 19:01:33 +0200

    feat: full d4

--- assets/scripts/Enemy.gd
+++ assets/scripts/Enemy.gd
@@ -11,7 +11,7 @@ var livingData : LivingEntity
 @export var acceleration : float
 @export var animationTree : AnimationTree
 @onready var navigation : NavigationAgent2D = $NavigationAgent2D
-
+var isRecievingDamage : bool
 
 func _ready() -> void:
 	if(livingData == null):
@@ -24,21 +24,33 @@ func _physics_process(delta: float) -> void:
 	aiMove(delta)
 
 func takeDamage(amount):
+	isRecievingDamage = true
+	animationTree.get("parameters/playback").travel("DAMAGE")
+	await get_tree().create_timer(0.3).timeout 
 	livingData.takeDamage(amount)
+	isRecievingDamage = false
 	if(livingData.currentHealth <= 0):
 		die()
+	
 
 func die():
 	queue_free()
 
 func aiMove(delta):
+	if(!isRecievingDamage):
+		animationTree.get("parameters/playback").travel("MOVE")
 	navigation.target_position = player.position
-	var direction = navigation.get_next_path_position() - global_position
-	direction = direction.normalized()
-	velocity = velocity.lerp(direction*speed,acceleration * delta)
+	velocity = velocity.lerp(getDirectionToPlayer()*speed,acceleration * delta)
 	if(getDistanceToPlayer() < maxFollowDistance):
 		move_and_slide()
-		animationTree.set("parameters/blend_position",direction)
+		animationTree.set("parameters/MOVE/blend_position",getDirectionToPlayer())
+		animationTree.set("parameters/DAMAGE/blend_position",getDirectionToPlayer())
 
+
+func getDirectionToPlayer():
+	var direction = navigation.get_next_path_position() - global_position
+	direction = direction.normalized()
+	return direction
+	
 func getDistanceToPlayer():
 	return global_position.distance_to(player.global_position)
--- assets/scripts/d4.gd
+++ assets/scripts/d4.gd
@@ -1,1 +1,34 @@
 extends Enemy
+
+@export var attackDistance : float
+var attackSpeedMultiplier : int = 500.5
+var isAttacking : bool
+@onready var animationPlayer : AnimationPlayer = $Sprite2D/AnimationPlayer
+
+func _physics_process(delta): 
+	if(getDistanceToPlayer() < maxFollowDistance):
+		move_and_slide()
+	if(getDistanceToPlayer() <= attackDistance && !isAttacking):
+		attack()
+	elif(!isAttacking):
+		super._physics_process(delta)
+
+func attack():
+	if(!isAttacking):
+		isAttacking = true
+		var playerDirection = player.global_position
+		velocity = Vector2.ZERO
+		await get_tree().create_timer(0.1).timeout
+		
+		var backDirection = -getDirectionToPlayer()
+		velocity = backDirection * (speed * 0.9)
+		await get_tree().create_timer(0.3).timeout
+		
+		var chargeDirection = (playerDirection - global_position).normalized()
+		velocity = chargeDirection * attackSpeedMultiplier
+		await get_tree().create_timer(0.6).timeout
+		isAttacking = false
+
+func takeDamage(damage):
+	super.takeDamage(damage)
+	
