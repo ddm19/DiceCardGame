e0f585afc339c18b33074ebc8364d04cbc1a778a
dan95 el guay <ddm19dev@gmail.com>
2025-04-12 13:38:12 +0200

    fix: attack animation collision

--- assets/cards/basicMeleeCard.gd
+++ assets/cards/basicMeleeCard.gd
@@ -3,13 +3,12 @@ extends Card
 @onready var attackSprite = $"AttackSprite"
 @export var animationName = "Slash"
 @export var damage = 10
-@onready var collisionShape = $Area2D
+@onready var collisionShape = $AttackSprite/Area2D
 var lastDirection : Vector2
 
 
 func _physics_process(delta: float) -> void:
 	attackSprite.global_position = player.global_position + lastDirection*15
-	collisionShape.global_position = player.global_position + lastDirection * 15
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	if direction != Vector2.ZERO:
 		lastDirection = direction
@@ -18,7 +17,6 @@ func _physics_process(delta: float) -> void:
 
 func doAttack():
 	attackSprite.rotation = lastDirection.angle()+90
-	collisionShape.rotation = lastDirection.angle()*90
 	player.updateAnimationsDirection(lastDirection)
 	if(attackSprite != null):
 		animationPlayer.play(animationName)
